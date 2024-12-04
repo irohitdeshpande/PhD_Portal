bb3be862294bc714f5a5c93c8c4dba28eb1ac5a2
Craig Rosario <craig.rosario04@gmail.com>
2024-12-04 23:27:15 +0530

    Included sidebar files with Dashboard and Thesis

--- PhD_Portal/src/App.jsx
+++ PhD_Portal/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from './LoginPage/LoginPage.jsx'
+import Dashboard from './Components/Dashboard/Dashboard.jsx';
 
 const App = () => {
   return (
     <>
-      
+      <Router>
+        <Routes>
+          <Route path='/' element={<LoginPage/>}></Route>
+          <Route path='/dashboard' element={<Dashboard/>}></Route>
+
+        </Routes>
+      </Router>
     </>
   )
 }
--- PhD_Portal/src/Components/LoginPage/LoginPage.jsx
+++ PhD_Portal/src/Components/LoginPage/LoginPage.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import './LoginPage.css'
 import SomaiyaLogo from '../assets/SomaiyaLogo.jpg';
 import Ganesh from '../assets/Ganesh.png'
 const LoginPage = () => {
+  const navigate=useNavigate();
+  const handleLogin=()=>{
+    navigate('/dashboard')
+  };
+
+
   return (
     <>
       <div className='LoginPage'>
         <div className='LoginSide'>
             <img src={SomaiyaLogo} alt="Somaiya Logo" className='SomaiyaLogo'/>
             Login to Portal!
-            <button className='LoginBtn'>Login</button>
+            <button className='LoginBtn' onClick={handleLogin}>Login</button>
         </div>
         <div className='ImageSide'>
           <img src={Ganesh} alt="Ganesh img" className='GaneshImg'/>
